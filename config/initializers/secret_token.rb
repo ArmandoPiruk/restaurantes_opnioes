0b61112c219c0d32965465373f9a563a55f08d2f
Armando <piruk7@gmail.com>
2011-02-26 19:21:21 -0300

    Testando integrcao GIT/HEROKU

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RestaurantesOpnioes::Application.config.secret_token = '40e4482d0058b455e6e4cecc81dde5c689c13a0cf21d275d4c4e5ecabefd9d75fdbde60da1803e27dc8c0c6c6ab98024c80657b9ea8ae119291ee8ea58f546eb'
