2ec4cd3c17f30ff57dad843a0e164bf616cdb19f
unknown <noote@.(none)>
2011-02-27 20:40:12 -0300

    Models OK

---
+++ app/models/restaurante.rb
@@ -0,0 +1,18 @@
+class Restaurante < ActiveRecord::Base
+	validates_presense_of :nome, :message => "deve ser preenchido"
+	validates_presense_of :endereco, :message => "deve ser preenchido"
+	validates_presense_of :especialidade, :message => "deve ser preenchido"
+	
+	validates_uniqueness_of :nome, :message => "nome já cadastrado"
+	validates_uniqueness_of :endereco, :message => "endereco já cadastrado"
+	
+	validate :primeira_letra_deve_ser_maiuscula
+	
+	private
+	def primeira_letra_deve_ser_maiuscula
+		errors.add("nome","primeira letra deve ser MAISCULA") unless nome =~ /[A-Z].*/
+	end
+	
+	has_many :qualificacoes
+	has_and_belongs_to_many :pratos
+end
